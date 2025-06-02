fda7288eafbd46dd2706c8f0cd4b43e2b42393c2
MeishaPD <putradewanm@student.ub.ac.id>
2025-06-02 10:24:27 +0700

    feat: integrate OneSignal for push notification

--- app/src/main/java/brawijaya/example/purisaehomestay/PuriSaeHomestayApp.kt
+++ app/src/main/java/brawijaya/example/purisaehomestay/PuriSaeHomestayApp.kt
@@ -1,7 +1,49 @@
 package brawijaya.example.purisaehomestay
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
-class PuriSaeHomestayApp : Application()
+class PuriSaeHomestayApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseApp.initializeApp(this)
+
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+
+        val db = FirebaseFirestore.getInstance()
+        val configRef = db.collection("config").document("onesignal")
+
+        configRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.contains("api_key")) {
+                    val apiKey = document.getString("api_key")
+                    if (!apiKey.isNullOrEmpty()) {
+                        OneSignal.initWithContext(this, apiKey)
+
+                        Log.d("PuriSaeApp", "Successfully initialize OneSignal")
+                        CoroutineScope(Dispatchers.IO).launch {
+                            OneSignal.Notifications.requestPermission(true)
+                            Log.d("PuriSaeApp", "Successfully subscribe to OneSignal notification")
+                        }
+                    } else {
+                        Log.e("PuriSaeApp", "OneSignal API Key is empty")
+                    }
+                } else {
+                    Log.e("PuriSaeApp", "OneSignal config not found")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("PuriSaeApp", "Failed to get OneSignal config", e)
+            }
+    }
+}
